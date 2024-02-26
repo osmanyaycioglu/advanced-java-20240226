4db48c9ca29badeff4c9b3e674fb880312927346
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-02-26 11:39:28 +0300

    Initial commit

---
+++ src/test/java/training/advanced/java/advanced/java/AdvancedJava20240226ApplicationTests.java
@@ -0,0 +1,13 @@
+package training.advanced.java.advanced.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedJava20240226ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
