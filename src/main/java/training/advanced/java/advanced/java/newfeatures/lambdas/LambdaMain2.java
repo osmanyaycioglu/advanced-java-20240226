e9b91668f625af5ce9c6edcd49309a72b18a46e9
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-02-28 14:31:48 +0300

    day 3 3

--- src/main/java/training/advanced/java/advanced/java/newfeatures/lambdas/LambdaMain2.java
+++ src/main/java/training/advanced/java/advanced/java/newfeatures/lambdas/LambdaMain2.java
@@ -1,8 +1,23 @@
 package training.advanced.java.advanced.java.newfeatures.lambdas;
 
+import java.util.Scanner;
+
 public class LambdaMain2 {
     public static void main(String[] args) {
         IProcess<String,Integer> processStr = s -> s.length();
         IProcess<String,String> processStr2 = s -> s + " processed";
+
+        try (Scanner scannerLoc = new Scanner(System.in)) {
+            System.out.println("bana sayı ver : ");
+            double vLoc = scannerLoc.nextDouble();
+            System.out.println("girdiğiniz sayı : " + vLoc);
+            System.out.println("bana başka sayı ver : ");
+            double vLoc2 = scannerLoc.nextDouble();
+            System.out.println("Toplamı : " + (vLoc + vLoc2));
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
